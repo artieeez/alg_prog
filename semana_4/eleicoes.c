d1a920d5594f765e9d6a91bc19ae93ff3dca0cbb
Artur <arturwodroid@gmail.com>
2021-08-30 15:42:16 -0300

    .

--- semana_4/eleicoes.c
+++ semana_4/eleicoes.c
@@ -8,12 +8,12 @@ void print_horizontal_line(int width, int solid);
 void print_padded_border(int padding_size, int left);
 void print_centered_string(char *str, int width);
 void print_cell(char *str, int width, char align);
-void print_table(int *vote_count);
+void print_table(int *vote_count, int tem_vencedor, int tem_empate, int votos_totais, int vencedor_index, int segundo_index);
 
 /* Constantes */
 #define TABLE_CHAR '*'
 #define COLUMN_WIDTH 10
-#define COLUMN_AMOUNT 4
+#define COLUMN_AMOUNT 5
 #define TABLE_PADDING 4
 const int TABLE_WIDTH = COLUMN_WIDTH * COLUMN_AMOUNT + TABLE_PADDING * 2;
 
@@ -23,10 +23,22 @@ const int TABLE_WIDTH = COLUMN_WIDTH * COLUMN_AMOUNT + TABLE_PADDING * 2;
 int main(void) {
     int voto = 5;
     char continuar = 's';
-    int vote_count[4] = {0, 0, 0, 0};
+    int vote_count[5] = {0, 0, 0, 0, 0};
+    int vencedor_index = 1;
+    int segundo_index = 2;
+    int votos_totais = 0;
+    int tem_vencedor = 0;
+    int tem_empate = 0;
     while(continuar == 's') {
-        
-        print_table(&vote_count);
+        /* Verifica situacao da votacao */
+        tem_vencedor = vote_count[vencedor_index] > ((float) votos_totais / 2)
+            ? 1 
+            : 0;
+        tem_empate = vote_count[vencedor_index] == vote_count[segundo_index]
+            ? 1 
+            : 0;
+
+        print_table(&vote_count, tem_vencedor, tem_empate, votos_totais, vencedor_index, segundo_index);
 
         /* 
             Coleta do voto */
@@ -39,11 +51,24 @@ int main(void) {
         if (voto < 0 || voto > 3) {
             clear_screen();
             printf("VOTO INVALIDO - Aperte enter para continuar.\n", voto);
+            vote_count[4]++;
             getchar();
             continue;
         }
-        vote_count[voto]++;
 
+        /* Contabiliza os votos */
+        vote_count[voto]++;
+        votos_totais++;
+        /* Atualiza a primeira e segunda colocação */
+        if (voto > 0 && voto < 4) {
+            if (vote_count[voto] > vote_count[vencedor_index] ||
+                (vote_count[voto] == vote_count[vencedor_index] && voto != vencedor_index)) {
+                segundo_index = vencedor_index;
+                vencedor_index = voto;
+            } else if (vote_count[voto] > vote_count[segundo_index] && voto != vencedor_index) {
+                segundo_index = voto;
+            }
+        }
         /* 
             Continuar? */
         do {
@@ -53,7 +78,7 @@ int main(void) {
         } while(continuar != 's' && continuar != 'n');
         voto = 5;
     }
-
+    print_table(&vote_count, tem_vencedor, tem_empate, votos_totais, vencedor_index, segundo_index);
     
 }
 
@@ -142,7 +167,7 @@ void print_cell(char *str, int width, char align)
 }
 
 /* Imprime tabela */
-void print_table(int *vote_count) {
+void print_table(int *vote_count, int tem_vencedor, int tem_empate, int votos_totais, int vencedor_index, int segundo_index) {
     clear_screen();
         print_horizontal_line(TABLE_WIDTH, 1);
         print_horizontal_line(TABLE_WIDTH, 0);
@@ -155,26 +180,45 @@ void print_table(int *vote_count) {
         print_cell("Chapa 1", COLUMN_WIDTH, 'r');
         print_cell("Chapa 2", COLUMN_WIDTH, 'r');
         print_cell("Chapa 3", COLUMN_WIDTH, 'r');
+        print_cell("Nulos", COLUMN_WIDTH, 'r');
         print_padded_border(TABLE_PADDING, 0); // Padded border
 
         print_padded_border(TABLE_PADDING, 1); // Padded border
-        for (int i = 0; i < 4; i++) {
-            char str[] = {'0' + vote_count[i], '\0'};
+        for (int i = 0; i < 5; i++) {
+            char str[5];
+            sprintf(str, "%d", vote_count[i]);
             print_cell(str, COLUMN_WIDTH, 'r');
         }
         print_padded_border(TABLE_PADDING, 0); // Padded border
         print_horizontal_line(TABLE_WIDTH, 1);
 
         print_padded_border(TABLE_PADDING, 1); // Padded border
-        print_cell("Situacao:", COLUMN_WIDTH, 'r');
-        print_cell("", COLUMN_WIDTH, 'r');
-        print_cell("", COLUMN_WIDTH, 'r');
-        print_cell("", COLUMN_WIDTH, 'r');
+
+        if (tem_vencedor && votos_totais >= 2) {
+            print_cell("Vencedor:", COLUMN_WIDTH, 'r');
+            printf("             Chapa %d | %6.2f%% dos votos",
+                vencedor_index,
+                ( vote_count[vencedor_index] / (float) votos_totais ) * 100
+            );
+        } else if(tem_empate && votos_totais >= 2) {
+            print_cell("Empate entre:", COLUMN_WIDTH, 'r');
+            print_cell("", COLUMN_WIDTH, 'r');
+            print_cell("", COLUMN_WIDTH, 'r');
+            printf("Chapa %d e Chapa %d",
+                vencedor_index, segundo_index);
+        } else if(votos_totais >= 2) {
+            print_cell("Segundo turno entre:", COLUMN_WIDTH, 'r');
+            print_cell("", COLUMN_WIDTH, 'r');
+            printf("   Chapa %d e Chapa %d",
+                vencedor_index, segundo_index);
+        } else {
+            print_cell("", COLUMN_WIDTH, 'r');
+            print_cell("", COLUMN_WIDTH, 'r');
+            print_cell("", COLUMN_WIDTH, 'r');
+            print_cell("", COLUMN_WIDTH, 'r');
+            print_cell("", COLUMN_WIDTH, 'r');
+        }
+
         print_padded_border(TABLE_PADDING, 0); // Padded border
         print_horizontal_line(TABLE_WIDTH, 1);
-}
-
-/* Contabiliza votos */
-void count_votes(int *vote_count[4]) {
-    return;
 }
